7e32df7df8532b1b11f74bec17f0bd590159952b
Sisir Chhetri <sisirchhetri9@gmail.com>
2025-01-21 23:15:53 +0530

    Http Session and Cookie Changes

--- 06_w05_feebackApp/src/main/java/session/Request1Servlet.java
+++ 06_w05_feebackApp/src/main/java/session/Request1Servlet.java
@@ -1,6 +1,7 @@
 package session;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,26 +9,29 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 @WebServlet("/servlet1")
 public class Request1Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		Cookie[] cookieList = req.getCookies();
+		String username ="sisir";
 		
-		if(null != cookieList) {
-			for(Cookie cookie : cookieList) {
-				
-				if(cookie.getName().equals("username")) {
-					System.out.println(
-							String.format("Cookie Key -> %s %n Value : %s", 
-										  "username", cookie.getValue())
-							);
-				}
-				
-			}
-		}
+		Cookie cookie = new Cookie("username", username);
+		cookie.setMaxAge(5*60);
+		resp.addCookie(cookie);
+		
+		//HTTP SESSION -> Setting Data in HTTP Session.
+		HttpSession session = req.getSession();
+		session.setAttribute("userCode", UUID.randomUUID().toString());
+		session.setMaxInactiveInterval(10*60);
+		
+		resp.setContentType("text/html");
+		resp.getWriter().println("<h1>Cookie set successfully.</h1>");
+		
+		
+		
 	}
 
 	
--- 06_w05_feebackApp/src/main/java/session/Request2Servlet.java
+++ 06_w05_feebackApp/src/main/java/session/Request2Servlet.java
@@ -1,6 +1,7 @@
 package session;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,17 +9,53 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+@SuppressWarnings("serial")
 @WebServlet("/servlet2")
 public class Request2Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String username ="sisir";
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
 		
-		Cookie cookie = new Cookie("username", username);
-		cookie.setMaxAge(5*60);
-		resp.addCookie(cookie);
+		Cookie[] cookieList = req.getCookies();
+		var isUserPresentFlag = false;
+		
+		HttpSession session = req.getSession();
+		String code = (String) session.getAttribute("userCode");
+		
+		
+		if(null != cookieList) {
+			for(Cookie cookie : cookieList) {
+				
+				if(cookie.getName().equals("username")) {
+					printWriter.println(
+							"""							
+							<h1>Welcome : %s </h1>
+							<h1>Secert key : %s</h1>
+							
+							""".formatted(cookie.getValue(), code));
+					System.out.println(
+							String.format("Cookie Key -> %s %n Value : %s", 
+										  "username", cookie.getValue())
+							);
+					isUserPresentFlag = true;
+				}
+				
+			}
+		}
+		
+		
+		
+		if(!isUserPresentFlag) {
+			printWriter.println("""
+					<h1>User Not Present </h1>
+					<h1>Username Cookie Doesn't Exists</h1>
+					""");
+		}
+		
 		
 	}
 
